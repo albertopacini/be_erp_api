874724b83b50a4b694c9fe604d9175370056db2b
caiserrr <pacini.alberto87@gmail.com>
2020-09-27 21:26:44 +0200

    Fix path

--- src/utils/initDataSource.js
+++ src/utils/initDataSource.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const { connect: mssqlLoader } = require('./mssql');
-const { connect: mongodbLoader } = require('./mongodb');
+const { connect: mssqlLoader } = require('../loaders/mssql');
+const { connect: mongodbLoader } = require('../loaders/mongodb');
 const Order = require('../services/Order');
 
 async function initDataSource() {
